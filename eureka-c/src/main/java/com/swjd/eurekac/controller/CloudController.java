d0b1e942fc9891ff428b64187f084c96e67570ba
author <1425207657@qq.com>
2020-10-22 14:44:47 +0800

    测试

---
+++ eureka-c/src/main/java/com/swjd/eurekac/controller/CloudController.java
@@ -0,0 +1,14 @@
+package com.swjd.eurekac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CloudController {
+    @ResponseBody//返回字符串
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello sptingcloud!";
+    }
+}
